2a9de5306b36a2b377ebf4f0dd0cd57b4909b7ba
Elaine <113189403+Elaineeechen@users.noreply.github.com>
2025-11-12 15:10:44 -0500

    Update hwk8.py

--- hwk8.py
+++ hwk8.py
@@ -1,5 +1,5 @@
 # Name:       Quinn He, Elaine Chen, Emily Wang, Tanya Chen
-# Peers:      N/A
+# Peers:      Maggie Hollis (CSC TA)
 # References: N/A
 
 def get_initial_parents(graph:dict[str|None,dict[str,float]|None], initial:str) -> dict[str,str|None]|None:
@@ -17,7 +17,7 @@ def get_initial_parents(graph:dict[str|None,dict[str,float]|None], initial:str) 
 def get_initial_costs(graph:dict[str|None,dict[str,float]|None], initial:str) -> dict[str,float]|None:
     costs:dict[str,float] = {}
     for node in graph.keys():
-        if node != None and node != initial:
+        if node != None:
             costs[node] = float("inf")
     initial_neighbors = graph[initial]
     if initial_neighbors == None:   # Error Checking
@@ -53,19 +53,18 @@ def run_dijkstra(graph:dict[str|None,dict[str,float]|None], start:str, finish:st
         return []
     
     node = find_lowest_cost_node(costs, processed)
+    processed.append(start)
 
     while node is not None:
         cost = costs[node]
+
         neighbors = graph[node] 
         if neighbors is not None:
             for n in neighbors.keys():
-                if n == start or n in processed:
-                    continue
-                if n in costs:
-                    new_cost = cost + neighbors[n]
-                    if costs[n] > new_cost:
-                        costs[n] = new_cost 
-                        parents[n] = node
+                new_cost = cost + neighbors[n]
+                if costs[n] > new_cost:
+                    costs[n] = new_cost 
+                    parents[n] = node
         processed.append(node)
         node = find_lowest_cost_node(costs,processed)
 
@@ -86,15 +85,16 @@ def run_dijkstra(graph:dict[str|None,dict[str,float]|None], start:str, finish:st
 
 
 def main():
+    # Assumption: We implement our undirected graph such that the undirected edges are represented as two directed edges (one each way). 
     graph:dict[str|None,dict[str,float]|None] = {}
-    graph['Ford'] = {'Parking Lot':1, 'Gym':0.7, 'Menden hall':0, 'Bass':1, 'Seelye':0}
+    graph['Ford'] = {'Parking Lot':1, 'Gym':0.7, 'Menden':0, 'Bass':1, 'Seelye':0}
     graph['Parking Lot'] = {'Ford':1}
     graph['Gym'] = {'Ford':0.7, 'Sage':0.2}
-    graph['Menden hall']  = {'Ford':0, 'Sage':0}
-    graph['Bass'] = {'Ford':1, 'McConnell':0.1, 'Alumni Gym':0.1, 'Neilson':0.6, 'Burton-Sabin-Reed':0.1}
-    graph['Sage'] = {'Gym':0.2, 'Menden hall':0, 'Botanic Garden':0.7}
-    graph['Seelye'] = {'Ford':0, 'Alumni Gym':0.3, 'Hillyer':0.1}
-    graph['Alumni Gym'] = {'Seelye':0.3, 'Bass':0.1}
+    graph['Menden']  = {'Ford':0, 'Sage':0}
+    graph['Bass'] = {'Ford':1, 'McConnell':0.1, 'Alumnae Gym':0.1, 'Neilson':0.6, 'Burton-Sabin-Reed':0.1}
+    graph['Sage'] = {'Gym':0.2, 'Menden':0, 'Botanic Garden':0.7}
+    graph['Seelye'] = {'Ford':0, 'Alumnae Gym':0.3, 'Hillyer':0.1}
+    graph['Alumnae Gym'] = {'Seelye':0.3, 'Bass':0.1}
     graph['Hillyer'] = {'Seelye':0.1, 'Hatfield':0}
     graph['McConnell'] = {'Bass':0.1, 'Burton-Sabin-Reed':0.1}
     graph['Botanic Garden'] = {'Sage':0.7, 'Burton-Sabin-Reed':0.3}
@@ -105,30 +105,37 @@ def main():
     graph['Campus Center'] = {'Neilson':0.6, 'Wright':0.2}
 
     # print(f"From Ford to Neilson: {run_dijkstra(graph, 'Ford', 'Neilson')}")
-
     # print(f"From Ford to Botanic Garden: {run_dijkstra(graph, 'Ford', 'Botanic Garden')}")
-
     # print(f"From Bass to Campus Center: {run_dijkstra(graph, 'Bass', 'Campus Center')}")
-
     # print(f"From Ford to Hatfield: {run_dijkstra(graph, 'Ford', 'Hatfield')}")
-
     # print(f"From Ford to Bass: {run_dijkstra(graph, 'Ford', 'Bass')}")
-
     # print(f"From Ford to Ford: {run_dijkstra(graph, 'Ford', 'Ford')}")
-
     # print(f"From Ford to X: {run_dijkstra(graph, 'Ford', 'X')}")
 
-    start = input("Enter the start node: ").strip()
-    finish = input("Enter the destination node: ").strip()
+    nodes = list(graph.keys())
+    print("Nodes: [", end="")
+    for i in range(len(nodes)):
+        print(nodes[i], end="")
+        if i != len(nodes) - 1:
+            print(", ", end="")
+    print("]")
 
-    if start not in graph or finish not in graph:
+    while True:
+        start = input("Enter the start node: ").strip()
+        finish = input("Enter the destination node: ").strip()
+        if start in graph and finish in graph:
+            break
         print("Error: one or both nodes are invalid.")
-        return
 
     path = run_dijkstra(graph, start, finish)
 
     if len(path) > 0:
-        print("Shortest path:", " -> ".join(path))
+        print("Shortest path:", end=" ")
+        for i in range(len(path)):
+            print(path[i], end="")
+            if i != len(path) - 1:
+                print(" -> ", end="")
+        print()
     else:
         print("No path is found.")
 
